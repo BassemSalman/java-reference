f1bbcda0dd4a02557486cb3c91a13538c9739f50
Bassem Salman <90832505+BassemSalman@users.noreply.github.com>
2023-06-28 15:36:54 +0300

    Add files via upload

---
+++ src/DataStructures/ArrayListRemoveNote.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListRemoveNote {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        int removedElement = list.remove(1); // removes at index 1, throws IndexOutOfBounds
+        boolean removedFlag = list.remove((Integer)1); // removes object 1
+        System.out.println(list);
+    }
+}
